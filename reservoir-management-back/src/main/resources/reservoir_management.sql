f530a6c1c1615a9015dd01cc344923734c063644
Tempted <2374704101@qq.com>
2023-10-18 22:27:24 +0800

    添加了数据库sql文件

---
+++ reservoir-management-back/src/main/resources/reservoir_management.sql
@@ -0,0 +1,23 @@
+# 用户模块
+# 用户登陆表r
+create table t_user
+(
+    id       int auto_increment primary key,
+    username varchar(50) not null,
+    password varchar(50) not null
+);
+
+# 登陆信息校验表
+create table t_token
+(
+    id      int auto_increment primary key,
+    user_id int         null,
+    token   varchar(50) null
+);
+
+# 用户详细信息表
+create table t_info
+(
+    id int auto_increment primary key
+
+);
