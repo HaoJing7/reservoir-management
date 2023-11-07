a6aba54ca4ded0a52b013f23fb3b762b1af8a1d4
wxjzdhhk <hellogit@163.com>
2023-11-07 17:06:19 +0800

    增加了自定义json格式，使得返回时间数据按照要求格式

--- reservoir-management-back/src/main/java/com/back/reservoirmanagement/config/WebMvcConfig.java
+++ reservoir-management-back/src/main/java/com/back/reservoirmanagement/config/WebMvcConfig.java
@@ -1,11 +1,14 @@
 package com.back.reservoirmanagement.config;
 
 import com.back.reservoirmanagement.interceptor.JwtTokenUserInterceptor;
+import com.back.reservoirmanagement.json.JacksonObjectMapper;
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -16,6 +19,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -41,6 +46,19 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
                 .excludePathPatterns("/app/**");
     }
 
+    /**
+     * 扩展mvc框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //设置对象转换器，底层使用Jackson将Java对象转为json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将上面的消息转换器对象追加到mvc框架的转换器集合中
+        converters.add(0,messageConverter);
+    }
 
     /**
      * 配置静态资源映射
